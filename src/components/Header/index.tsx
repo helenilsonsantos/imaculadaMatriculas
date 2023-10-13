3fc1c9071678858f2be8a0db606f7c180bd03e24
Helenilson Holamek <helenilson@meuhg.com>
2023-10-13 15:37:37 -0300

    refactor: redirecionando o link para o formulário

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,15 @@
 import logoCic from '../../assets/img/logo.png'
+import matriculasImg from '../../assets/img/matriculas.png'
 import './header.css'
 
 function Header () {
 
-    const scrollToFormMatriculas = () => {
+    const formMatriculas = () => {
 
-        const elementoAncora = document.getElementById('formMatriculas')
+        const formActivesoft = 'https://siga03.activesoft.com.br/captacao_alunos/?codigo=CIC_OLHO&unidade=1'
 
-        if (elementoAncora) {
-          elementoAncora.scrollIntoView({ behavior: 'smooth' });
-        }
+        return window.open(formActivesoft, '_blank')
+
     }
 
     return (
@@ -19,15 +19,15 @@ function Header () {
                 <span>
                     <h1>MATRÍCULAS 2024</h1>
                     <p>Está <b>aberto</b> o período para realização das matrículas para o ano letivo de <b>2024</b>.</p>
-                    <p>Preencha o formulário abaixo. Entraremos em contato o mais rápido possível. </p>
-                    <button className='btnDefault btnMatriculas2024' onClick={scrollToFormMatriculas}>PRÉ - MATRÍCULA</button>
+                    <p>Realize a sua pré-matrícula, clicando abaixo. Entraremos em contato o mais rápido possível. </p>
+                    <button className='btnDefault btnMatriculas2024' onClick={formMatriculas}>PRÉ - MATRÍCULA</button>
                 </span>
 
                 <span className='imgAluno'>
-
+                    <img id='matriculasAbertas' src={matriculasImg} alt='Matrículas 2024. Colégio Imaculada Conceição'/>
                 </span>
             </span>
-            <button className='btnDefault btnPreMatricula' onClick={scrollToFormMatriculas}>PRÉ - MATRÍCULA</button>
+            <button className='btnDefault btnPreMatricula' onClick={formMatriculas}>PRÉ - MATRÍCULA</button>
         </section>
     )
 }
